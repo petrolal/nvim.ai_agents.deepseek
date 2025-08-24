b0c64021c83dce64bad0fe9b0b8e0f51fe638ed3
petrolal <petrolalucas@gmail.com>
2025-08-24 19:58:37 -0300

    fix:Name corrections

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,10 +31,12 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
+
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
+
   performance = {
     rtp = {
       -- disable some rtp plugins
